6e2653bc9b4f9a83f02864020039f4375bf7ee17
BAkanksha <akankshasingh4668@gmail.com>
2024-10-07 12:25:52 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,6 +3,9 @@ const getTotalIsles = function (grid) {
 
   // write your code here
   if(grid || grid.length==0){
+    const row =grid.length;
+    const col = grid[0].length;
+    lrt islandCount=0;
     
   }
 
