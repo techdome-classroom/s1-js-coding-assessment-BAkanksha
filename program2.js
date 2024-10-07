6f144836d280da4a6a104736d8ca20cc3dbfd863
BAkanksha <akankshasingh4668@gmail.com>
2024-10-07 12:39:39 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -11,7 +11,14 @@ const decodeTheRing = function (s, p) {
       }
     }
     for(let i=1;i<=n; i++){
-      
+      for(let j=1; j<=m; j++){
+        if(p[i-1] == s[j-1] || p[i-1] == '?'){
+          dp[i][j] = dp[i-1][j-1];
+        }
+        else if(p[i-1] == '*'){
+          dp[i][j] = dp[i-1][j] || dp[i][j-1];
+        }
+      }
     }
 
   };
